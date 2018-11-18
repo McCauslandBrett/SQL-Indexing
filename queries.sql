a7aa660a5c09f54f9ec83e4c9711f0d68188a882
brettmccausland <brettmccausland@yahoo.com>
2018-11-17 18:06:55 -0800

    s

--- queries.sql
+++ queries.sql
@@ -19,19 +19,21 @@ where part_nyc.on_hand>70;
 -- 3. List all the suppliers that have more total
 -- on hand parts in NYC than they do in SFO.
 Select supplier
-FROM (
+FROM
   Select sfoWITHsum.supplier,sfoWITHsum.partsum
   FROM (select part_sfo.supplier,SUM(part_sfo.on_hand) as partsum
         from part_sfo
-        group by supplier;
+        group by supplier
       ) as sfoWITHsum
       ,
+
       Select nycWITHsum.supplier,nycWITHsum.partsum
       FROM (select part_nyc.supplier,SUM(part_nyc.on_hand) as partsum
-        from part_nyc
-        group by supplier
-      ) as nycWITHsum
-    )
+            from part_nyc
+             group by supplier
+            ) as nycWITHsum
+
+    
 where sfoWITHsum.supplier = nycWITHsum.supplier
 And sfoWITHsum.partsum < nycWITHsum.partsum;
       -- UNION
