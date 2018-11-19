4c1890d966d705ae894b38a8e5b8c43b9d3ef5ca
brettmccausland <brettmccausland@yahoo.com>
2018-11-18 18:48:11 -0800

    s

--- queries.sql
+++ queries.sql
@@ -4,17 +4,17 @@ from part_nyc
 where part_nyc.on_hand>70;
 
 -- 2. Count how many total parts on hand, in both NYC and SFO, are Red
-
-Select t1.count
-FROM (select count(*)
-      from part_sfo,color
-      where part_sfo.color=color.color_id and color.color_name='Red'
-      UNION
-      select count(*)
-      from part_nyc,color
-      where part_nyc.color=color.color_id and color.color_name='Red'
-      )
-      AS t1
+-- 
+-- Select t1.count
+-- FROM (select count(*)
+--       from part_sfo,color
+--       where part_sfo.color=color.color_id and color.color_name='Red'
+--       UNION
+--       select count(*)
+--       from part_nyc,color
+--       where part_nyc.color=color.color_id and color.color_name='Red'
+--       )
+--       AS t1
 
 -- 3. List all the suppliers that have more total
 -- on hand parts in NYC than they do in SFO.
